764d0fed5d55a008fa7364dd386dbb4f081b7f07
tahaanik <tahaanik729@gmail.com>
2026-09-05 11:06:38 +0530

    added connnections

--- app/database/connection.py
+++ app/database/connection.py
@@ -1,14 +1,23 @@
 import os
 from dotenv import load_dotenv
 load_dotenv()
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine,text
 from sqlalchemy.orm import sessionmaker
 
 db_url = os.getenv("DBURL")
+if not db_url:
+    raise RuntimeError('DatabaseUrl not found ')
 
-engine = create_engine(db_url)
+engine = create_engine(db_url,pool_pre_ping=True)
 
-SessionLocal = sessionmaker(bind=engine)
-db = SessionLocal()
-with engine.connect() as conn:
-    print(f'suceeessss{db_url}')
+SessionLocal = sessionmaker(bind=engine,autoflush=False,expire_on_commit=False)
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db 
+    finally:
+        db.close()
+if __name__ == '__main__':
+    with engine.connect() as conn:
+        result = conn.execute(text('SELECT 5')).scalar_one()
+        print(f'Database created received {result}')
